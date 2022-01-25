372d3c7ae5c119a490ec59ec933e58712ff984a8
ShadowPrince <zhek4770@gmail.com>
2022-01-25 23:54:45 +0200

    Fix the link to static files

--- src/index.js
+++ src/index.js
@@ -12,27 +12,28 @@ const io = socketio(server);
 const port = process.env.PORT || 3000;
 
 // Setting up a static folder
-const publicDirectoryPath = path.join(__dirname, '../public');
+const publicDirectoryPath = path.join(__dirname, '..');
 app.use(express.static(publicDirectoryPath));
 
 // Listen to the client connection
 io.on('connection', (socket) => {
+  // Client join event
+  socket.on('joinInitMap', () => {
+    console.log(
+      chalk.bold.white.bgBlue(`We have a new connection: ${socket.id}!`)
+    );
 
-    // Client join event
-    socket.on('joinInitMap', () => {
-        console.log(chalk.bold.white.bgBlue(`We have a new connection: ${socket.id}!`));
+    socket.emit('consoleMessage', 'You were connected to the socket');
+  });
 
-        socket.emit('consoleMessage', 'You were connected to the socket');
-    })
-
-    socket.on('updateGeolocation', ({ longitude, latitude }) => {
-        console.log(longitude, latitude)
-        id = socket.id;
-        socket.broadcast.emit('updatePoint', ({ longitude, latitude, id }))
-    })
-})
+  socket.on('updateGeolocation', ({ longitude, latitude }) => {
+    console.log(longitude, latitude);
+    id = socket.id;
+    socket.broadcast.emit('updatePoint', { longitude, latitude, id });
+  });
+});
 
 // Listen to the server
 server.listen(port, () => {
-    console.log('Server is up on port ' + port);
+  console.log('Server is up on port ' + port);
 });
